75b8c839248e3a44c2ea3c84b2a1a02d77260d73
karthikaprakash08 <karthikaprakash008@gmail.com>
2024-10-25 21:05:42 +0530

    api

--- frontend/src/LandingPage/Courses/CoursesLandingPage.jsx
+++ frontend/src/LandingPage/Courses/CoursesLandingPage.jsx
@@ -26,7 +26,7 @@ const CoursesLandingPage = () => {
     const fetchData = async () => {
       try {
         const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
-        const response = await axios.get(`https://csuite-ui0f.onrender.com/api`);
+        const response = await axios.get(`https://csuite-ui0f.onrender.com`);
         const allCourses = response.data;
 
         // filtering purchased course
